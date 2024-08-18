82855ed013af0d89efab59270be674c41862f496
RajDhakal <64518171+Raj5585@users.noreply.github.com>
2024-08-19 00:16:37 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ const theme = createTheme();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+   <React.StrictMode>
   <ThemeContextProvider>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </ThemeContextProvider>
+</React.StrictMode>
 );
 
 reportWebVitals();
+
