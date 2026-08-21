4c4dec70598a14eb9711ed9193502db326cdc630
Raj Kumar Dhakal <raj@Rajs-MacBook-Pro.local>
2026-08-21 21:33:39 +0545

    Restore contact email

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,5 +1,5 @@
 export const contactsData = {
-    email: 'rajdhakal.dn@gmail.com',
+    email: 'rajdhakal.404@gmail.com',
     phone: '+9779840327380',
     address: 'Nuwakot,Nepal',
     github: 'https://github.com/Raj5585',
